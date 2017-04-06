332057262357aae49ae2f741ee1f384554c3ad27
xian <pzhxbz@QQ.com>
2017-04-06 18:57:32 +0800

    捕获链接超时的异常

--- getPhoto.py
+++ getPhoto.py
@@ -72,6 +72,9 @@ def get_photo_from_url(Url, id, word):
     except requests.exceptions.ConnectionError:
         print('【错误】当前图片无法下载')
         return
+    except socket.timeout:
+        print('链接超时')
+        return
     string = 'pictures/' + word + '_' + str(id) + '.jpg'
     fp = open(string.encode('cp936'), 'wb')
     fp.write(pic.content)
