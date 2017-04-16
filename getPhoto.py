20f416b09a7fc07c371fa42926b133f66fe6c1ae
Tansong <tansongsong@gmail.com>
2017-04-16 15:53:00 +0800

    修正json解析

--- getPhoto.py
+++ getPhoto.py
@@ -16,55 +16,60 @@ header = {
 }
 
 
-def get_photo(word, size):
+def get_photo(words, size):
     try:
         os.mkdir("pictures")
     except FileExistsError:
         pass
     tasks = []
-    for i in range(int(size/30)):
-        params = {
-            'tn':'resultjson_com',
-            'ipn':'rj',
-            'ct':'201326592',
-            'is':'',
-            'fp':'result',
-            'queryWord':word,
-            'cl':'2',
-            'lm':'-1',
-            'ie':'utf-8',
-            'oe':'utf-8',
-            'adpicid':'',
-            'st':'-1',
-            'z':'',
-            'ic':'0',
-            'word':word,
-            's':'',
-            'se':'',
-            'tab':'',
-            'width':'',
-            'height':'',
-            'face':'0',
-            'istype':'2',
-            'qc':'',
-            'nc':'1',
-            'fr':'',
-            'pn':str(i*30),   #start index
-            'rn':'30',  #num
-            'gsm':'5a',
-            '1491273347594':''
-        }
-        z1 = requests.get(url=url, params=params, headers=header)
-        print(z1.status_code)
-        js = z1.json()
-        temp = js['data']
-
-        for j in range(30):
-            each = temp[j]['objURL']
-            each = decode(each)
-            # get_photo_from_url(each, i*30+j, word)
-            tasks.append((each, i * 30 + j, word))
-    pool = ThreadPool(30)
+    for word in words:
+        for i in range(int(size/30)):
+            params = {
+                'tn':'resultjson_com',
+                'ipn':'rj',
+                'ct':'201326592',
+                'is':'',
+                'fp':'result',
+                'queryWord':word,
+                'cl':'2',
+                'lm':'-1',
+                'ie':'utf-8',
+                'oe':'utf-8',
+                'adpicid':'',
+                'st':'-1',
+                'z':'',
+                'ic':'0',
+                'word':word,
+                's':'',
+                'se':'',
+                'tab':'',
+                'width':'',
+                'height':'',
+                'face':'0',
+                'istype':'2',
+                'qc':'',
+                'nc':'1',
+                'fr':'',
+                'pn':str(i*30),   #start index
+                'rn':'30',  #num
+                'gsm':'5a',
+                '1491273347594':''
+            }
+            z1 = requests.get(url=url, params=params, headers=header)
+            print(z1.status_code)
+            try:
+                js = z1.json()
+            except:
+                print('error and jump')
+                continue
+            temp = js['data']
+            print('get 30 ' + word + ' records')
+            for j in range(30):
+                each = temp[j]['objURL']
+                each = decode(each)
+                # get_photo_from_url(each, i*30+j, word)
+                tasks.append((each, i * 30 + j, word))
+    pool = ThreadPool(40)
     pool.starmap(get_photo_from_url, tasks)
     pool.close()
 
@@ -73,7 +78,7 @@ def get_photo_from_url(Url, id, word):
     print('正在下载第'+str(id)+'张图片')
     print(Url)
     try:
-        pic = requests.get(Url, timeout=5)
+        pic = requests.get(Url, timeout=4)
     except Exception as e:
         print('【错误】当前图片无法下载')
         return
